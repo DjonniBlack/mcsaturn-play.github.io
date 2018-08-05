9741446fb86f0324b580105172ad2459333209f0
AokI <amirxan833@gmail.com>
2018-08-06 01:33:29 +0500

    Initial commit

---
+++ mcdonate.ru/api/mcd_script24ca.js
@@ -0,0 +1,213 @@
+mcdApi = {
+
+	
+	"previes": [{"previ_id":887452,"price":25,"title":"\u0414\u043e\u043d\u0430\u0442-\u041a\u0435\u0439\u0441 (1 \u0448\u0442.)","descr":"\u0412\u044b\u0434\u0430\u0451\u0442 \u043a\u0435\u0439\u0441, \u0438\u0437 \u043a\u043e\u0442\u043e\u0440\u043e\u0433\u043e \u0432\u044b\u043f\u0430\u0434\u0430\u0435\u0442 \u0440\u0430\u043d\u0434\u043e\u043c\u043d\u044b\u0439 \u0434\u043e\u043d\u0430\u0442 \u043e\u0442 \u041f\u0440\u0435\u043c\u0438\u0443\u043c\u0430 \u0434\u043e \u0418\u043c\u043f\u0435\u0440\u0430\u0442\u043e\u0440\u0430.","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":887454,"price":50,"title":"\u0414\u043e\u043d\u0430\u0442-\u041a\u0435\u0439\u0441 (3 \u0448\u0442.)","descr":"\u0412\u044b\u0434\u0430\u0451\u0442 \u043a\u0435\u0439\u0441, \u0438\u0437 \u043a\u043e\u0442\u043e\u0440\u043e\u0433\u043e \u0432\u044b\u043f\u0430\u0434\u0430\u0435\u0442 \u0440\u0430\u043d\u0434\u043e\u043c\u043d\u044b\u0439 \u0434\u043e\u043d\u0430\u0442 \u043e\u0442 \u041f\u0440\u0435\u043c\u0438\u0443\u043c\u0430 \u0434\u043e \u0418\u043c\u043f\u0435\u0440\u0430\u0442\u043e\u0440\u0430.","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583788,"price":10,"title":"\u0412\u0438\u043f","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583791,"price":20,"title":"\u041f\u0440\u0435\u043c\u0438\u0443\u043c","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583792,"price":30,"title":"\u041a\u0440\u0435\u0430\u0442\u0438\u0432","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583793,"price":40,"title":"\u0410\u0434\u043c\u0438\u043d","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583794,"price":50,"title":"(S)\u0410\u0434\u043c\u0438\u043d","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583795,"price":60,"title":"\u041b\u043e\u0440\u0434","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583796,"price":70,"title":"\u0422\u0438\u0442\u0430\u043d","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583797,"price":100,"title":"\u0414\u0435\u043b\u044e\u043a\u0441","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583798,"price":150,"title":"\u0411\u043e\u0441\u0441","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583799,"price":200,"title":"\u042d\u043d\u0438\u0433\u043c\u0430","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583800,"price":500,"title":"\u041a\u043e\u0440\u043e\u043b\u044c","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583801,"price":999,"title":"\u041b\u0435\u0433\u0435\u043d\u0434\u0430","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583802,"price":1799,"title":"\u0418\u043c\u043f\u0435\u0440\u0430\u0442\u043e\u0440","descr":"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u0435\u0435 \u043e \u0434\u043e\u043d\u0430\u0442\u0435 \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u043a\u043e\u043c\u0430\u043d\u0434\u044b \/donate","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":583803,"price":3499,"title":"\u042d\u043d\u0434\u0435\u0440","descr":"\u0412\u0441\u0435 \u043f\u0440\u0430\u0432\u0430","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"},{"previ_id":584022,"price":100,"title":"\u0420\u0430\u0437\u0431\u0430\u043d \u043d\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0435 (\u041f\u043e IP \u0438 \u043e\u0431\u044b\u0447\u043d\u044b\u0439 \u0441\u0440\u0430\u0437\u0443)","descr":"\u0420\u0430\u0437\u0431\u0430\u043d \u0430\u043a\u043a\u0430\u0443\u043d\u0442\u0430","img_type":"","do_not_resize":0,"imageUrl":"http:\/\/mcdonate.ru\/static\/v2\/img\/default_previ.png"}],
+	"srv": {"server_id":"190017","domain":"mcsaturn","maxPlayers":150,"curPlayers":25},
+	"callback": "mcdCallback",
+	"container_id": "",
+	"nickname": "",
+	"beauty": 0,
+	
+	init: function(){
+		console.log('MCD API Enabled (' +  mcdApi.srv.server_id + ')');
+		
+		
+
+		if (mcdApi.callback.length > 3){
+
+			eval('var fn = ' + mcdApi.callback + ';');
+			fn.apply(null, [mcdApi.getInfo()]);		
+			
+		}
+		
+		if (mcdApi.container_id.length > 1){
+			if (mcdApi.beauty){
+				document.getElementById(mcdApi.container_id).setAttribute("class", 'beauty');
+				document.getElementById(mcdApi.container_id).innerHTML = mcdApi.printInputBeauty();
+				mcdApi.bindings();
+			}
+			else {
+				document.getElementById(mcdApi.container_id).innerHTML = mcdApi.printInput();			
+			}
+		}
+
+
+		
+	},
+	
+	bindings: function(){
+		var select = document.getElementById('MCDSelect');	
+		var text = document.getElementById('MCDSelectText');
+
+		select.onclick = function(e) {
+			e.preventDefault();
+			this.className = this.className.indexOf('MCDOpen') == -1 ? 'MCDOpen' : '';
+			
+			e = e || window.event;
+			var el = e.target || e.srcElement;
+
+			if(el.parentNode.id == 'MCDSelectDrop') {
+				text.innerHTML = el.innerHTML;
+				text.style.color = 'inherit';
+				var spans = el.parentNode.getElementsByTagName('span');
+				for(var i = 0; i < spans.length; i++)
+				{
+					spans[i].style.fontWeight = 'normal';
+				}
+				el.style.fontWeight = 'bold';
+				console.log(el.getAttribute('previ_id'));
+				this.setAttribute('previ_id', el.getAttribute('previ_id'));
+				mcdApi.selectedPrevi = el.getAttribute('previ_id');
+			}
+		};
+		
+	},
+	
+	printInput: function(){
+		var html = "<select class='mcdApiSelect'>";
+		
+		for (var i = 0 ; i < mcdApi.previes.length; i++){
+			var item = mcdApi.previes[i];
+			
+			html += "<option previ_id='" + item.previ_id + "' price='" + item.price + "'>" + item.title + " (" + item.price + " руб.)</option>";
+		}
+
+		html += '</select><input type="button" class="mcdApiButton" value="Купить" onClick="mcdApi.doBuyPrevi(this);">';
+		return html ;
+	},
+	
+	printInputBeauty: function(){
+		
+		var preselect = '';
+		var MCDSelectText = 'Выберите привилегию';
+		
+		if (mcdApi.previes.length == 1){
+			mcdApi.selectedPrevi = mcdApi.previes[0].previ_id;
+			preselect = 'previ_id="' + mcdApi.previes[0].previ_id + '"';
+			MCDSelectText = '<span style="color: #555;">' + mcdApi.previes[0].title + " (" + mcdApi.previes[0].price + " руб.) </span>" ;
+		}
+		
+		
+		var html = '<div id="MCDTitle">Купить донат</div><input type="text" id="MCDnickname" class="MCDinput" placeholder="Ваш никнейм в игре"><div id="MCDSelect" ' + preselect + '><div id="MCDSelectContent"><div id="MCDSelectText">' + MCDSelectText + '</div><div id="MCDSelectDrop">';
+		
+		for (var i = 0 ; i < mcdApi.previes.length; i++){
+			var item = mcdApi.previes[i];
+			
+			html += "<span previ_id='" + item.previ_id + "' price='" + item.price + "'>" + item.title + " (" + item.price + " руб.) </span> ";
+		}
+
+		html += '</div></div></div><div id="MCDbutton" onClick="mcdApi.setNickname(document.getElementById(\'MCDnickname\').value); mcdApi.buyPreviNative(mcdApi.selectedPrevi);">Продолжить</div>';
+		return html ;
+	},
+	
+	doBuyPrevi: function(_this){
+		
+		var select = _this.previousSibling  ;
+		var option = select[select.selectedIndex];
+		
+		var previ_id = option.getAttribute('previ_id');
+		
+		mcdApi.buyPreviNative(previ_id);
+
+	},
+	
+	buyPreviNative: function(previ_id){
+		var url = 'http://' + mcdApi.srv.domain + '.mcdonate.ru';	
+		
+		url = url + '/performOrder2/?mcdApi&server_id=' + mcdApi.srv.server_id + '&previ_id=' + previ_id;
+		
+		if (mcdApi.nickname.length > 0){
+			url += '&playerNickname=' + mcdApi.nickname;
+		}
+		
+		document.location.href = url ;		
+	},
+	
+	setNickname: function(nickname){
+		mcdApi.nickname = nickname ;
+	},
+	
+	getInfo: function(){
+		var text = mcdApi.previes;
+		
+		return text; 
+	},
+};
+
+function bindReady(handler){
+
+	var called = false
+
+	function ready() { // (1)
+		if (called) return
+		called = true
+		handler()
+	}
+
+	if ( document.addEventListener ) { // (2)
+		document.addEventListener( "DOMContentLoaded", function(){
+			ready()
+		}, false )
+	} else if ( document.attachEvent ) {  // (3)
+
+		// (3.1)
+		if ( document.documentElement.doScroll && window == window.top ) {
+			function tryScroll(){
+				if (called) return
+				if (!document.body) return
+				try {
+					document.documentElement.doScroll("left")
+					ready()
+				} catch(e) {
+					setTimeout(tryScroll, 0)
+				}
+			}
+			tryScroll()
+		}
+
+		// (3.2)
+		document.attachEvent("onreadystatechange", function(){
+
+			if ( document.readyState === "complete" ) {
+				ready()
+			}
+		})
+	}
+
+	// (4)
+    if (window.addEventListener)
+        window.addEventListener('load', ready, false)
+    else if (window.attachEvent)
+        window.attachEvent('onload', ready)
+    /*  else  // (4.1)
+        window.onload=ready
+	*/
+}
+
+readyList = []
+
+function onReady(handler) {
+
+	if (!readyList.length) {
+		bindReady(function() {
+			for(var i=0; i<readyList.length; i++) {
+				readyList[i]()
+			}
+		})
+	}
+
+	readyList.push(handler)
+}
+
+onReady(function() {
+	mcdApi.init();
+
+	var url = 'https://mcdonate.ru/api/';
+	var style = document.createElement('link');
+	style.href = url + 'api_style.css';
+	style.type = 'text/css';
+	style.rel = 'stylesheet';
+	document.getElementsByTagName('head')[0].appendChild(style);
+});
+
+
